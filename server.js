6ad0a11c1a4413a875c6a10ff4545fe324ac4ac8
Jonathan Thompson <jathompson399@gmail.com>
2022-08-24 19:29:55 -0500

    rows

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,20 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Carmine37!',
+    database: 'election'
+  },
+  console.log('Connected to the eleciton database.')
+);
+
+db.query(`SELECT * FROM canidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Not Found
 app.use((req, res) => {
